56f73dec312b820f5f75fb2c8de3829c332cf39b
HarkHorning <hjhorning@gmai.com>
2025-04-13 17:50:00 -0600

    connected gemini api to flask and template

--- connect_genai.py
+++ connect_genai.py
@@ -8,12 +8,13 @@ load_env = load_dotenv(envirenment)
 API_KEY = os.getenv("API_KEY")
 client = genai.Client(api_key=API_KEY)
 
-question = input("What would you like to ask Gemini? ")
+# question = input("What would you like to ask Gemini? ")
 
-async def response():
+async def response(ask):
+    question = ask
     response = client.models.generate_content(
         model="gemini-2.0-flash", contents=question
     )
-    print(response.text)
+    return response.text
 
-asyncio.run(response())
+# asyncio.run(response())
--- main.py
+++ main.py
@@ -1,9 +1,19 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
+import asyncio
+from connect_genai import response
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return render_template("index.html")
+def index():
+    answer = ""
+    return render_template("index.html", answer=answer)
+
+@app.route("/ask", methods=['POST'])
+def post():
+    ask = request.form.get('ask')
+    answer = asyncio.run(response(ask))
+    return render_template('index.html', answer=answer)
+
 
 app.run(host="127.0.0.1", port=3000)
