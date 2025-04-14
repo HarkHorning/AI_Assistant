219dded3cfbfe289ac5bcac54ac612b23383be93
HarkHorning <hjhorning@gmai.com>
2025-04-13 18:41:34 -0600

    cleaned up code a little

--- main.py
+++ main.py
@@ -4,16 +4,23 @@ from connect_genai import response
 
 app = Flask(__name__)
 
+
+# clean and process gemini responses
+def clean(gemini_response):
+    return jsonify(**gemini_response)
+
+
 @app.route("/")
 def index():
     answer = ""
     return render_template("index.html", answer=answer)
 
+
 @app.route("/ask", methods=['POST'])
 def post():
     ask = request.form.get('ask')
     gemini_response = asyncio.run(response(ask))
-    answer = jsonify(**gemini_response)
+    answer = clean(gemini_response)
     return render_template('index.html', answer=answer)
 
 
